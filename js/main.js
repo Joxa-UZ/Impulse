cbc295ebd965ff5b801a069806fee52bc7c67487
Bunyod <84246289+Bunyood@users.noreply.github.com>
2022-10-30 01:29:58 +0500

    bug fix

--- js/main.js
+++ js/main.js
@@ -28,3 +28,25 @@ buttons.forEach(button => {
         delete activeSlide.dataset.active
     })
 })
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav: true,
+    dots: false,
+    // autoplay:true,
+    // autoplayTimeout:1000,
+    // autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
